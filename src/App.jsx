c651dd6c38644cb3ef61dc93dad8c762e8b300b4
Alexey Koshelev <koschelev00@mail.ru>
2025-06-11 08:41:46 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,26 @@
 import "./App.css";
 import AppRoutes from "./AppRoutes";
 import { SGlobal } from "./Global.styled";
-import AuthProvider from './context/AuthProvider'
-import { CardsProvider } from './context/CardsProvider' 
-import { useState } from 'react'
-import { ThemeProvider } from './context/ThemeProvider'
-import { lightTheme, darkTheme } from './themes'
+import AuthProvider from "./context/AuthProvider";
+import { CardsProvider } from "./context/CardsProvider";
+import { useState } from "react";
+import { ThemeProvider } from "./context/ThemeProvider";
+import { lightTheme, darkTheme } from "./themes";
 
 function App() {
-    const [theme, setTheme] = useState(lightTheme)
-    return (
-        <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
-            <AuthProvider>
-                <CardsProvider> 
-                    <SGlobal />
-                    <AppRoutes />
-                </CardsProvider>
-            </AuthProvider>
-        </ThemeProvider>
-    )
+  const [theme, setTheme] = useState(lightTheme);
+  return (
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+        
+      <AuthProvider>
+        <CardsProvider>
+        <SGlobal/>
+        <AppRoutes />
+         </CardsProvider>
+      </AuthProvider>
+     
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
