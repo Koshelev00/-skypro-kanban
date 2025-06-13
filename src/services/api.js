b1eafe52d6128bee00278e7c8d2fa33e0b7812ba
Koshelev <koschelev00@mail.ru>
2025-06-13 15:53:05 +0300

    1

--- src/services/api.js
+++ src/services/api.js
@@ -13,7 +13,6 @@ export async function fetchCards({ token }) {
             },
         })
         return data.data.tasks
-        // когда работаем с axios, не забываем, что результат лежит в ключе datа
     } catch (error) {
         throw new Error(error.message)
     }
